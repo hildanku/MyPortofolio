95fc9220938e19f785ca77e0e0d90b23d8565927
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-06-23 11:31:44 +0700

    chore(App): widihhh

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-  <h1>wellcome</h1>
+  <div class="container">
+    <h1>Hildan K. Utomo</h1>
+  </div>
 </template>
 
 <style scoped>
-
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 </style>
