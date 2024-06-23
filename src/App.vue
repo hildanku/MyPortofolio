f8b83f15325db1e782bf28ca25a8c0f96170e21b
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-06-23 12:02:51 +0700

    feat: added primeicons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script setup>
-
+import Button from 'primevue/button';
 </script>
 
 <template>
   <div class="container">
     <h1>Hildan K. Utomo</h1>
+    <div class="button">
+      <Button icon="pi pi-linkedin" severity="info" text rounded aria-label="LinkedIn" />
+      <Button icon="pi pi-facebook" severity="info" text rounded aria-label="LinkedIn" />
+      <Button icon="pi pi-instagram" severity="help" text rounded aria-label="LinkedIn" />
+    </div>
   </div>
+  <!-- <Button label="LinkedIn" icon="pi pi-linkedin" severity="info" /> -->
+
 </template>
 
 <style scoped>
+.button {
+
+}
 .container {
+  flex-direction: column; /* Arrange items vertically */
   display: flex;
   justify-content: center;
   align-items: center;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css';
+import 'primeicons/primeicons.css'
+  //import preset        
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app')
